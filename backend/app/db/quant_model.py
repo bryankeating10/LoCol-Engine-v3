3741a398be47fda84f8e498287c488982391d589
Bryan Keating <ru.bryankeating@gmail.com>
2026-03-27 08:27:37 -0400

    Quant model with testers relationship

--- backend/app/db/quant_model.py
+++ backend/app/db/quant_model.py
@@ -1,1 +1,13 @@
-from app.db.base import Base
+from app.db import Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship as rel
+
+class Quant(Base):
+    __tablename__ = 'quant'
+
+    # Identification
+    id = Column(Integer, primary_key=True)
+    name = Column(String, unique=True)
+
+    # Relationships
+    testers = rel('Tester', back_populates='quant')
