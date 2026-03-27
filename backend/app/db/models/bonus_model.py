129d2d055265f4b6a1ce4f1a8d58ec862d7c5d74
Bryan Keating <ru.bryankeating@gmail.com>
2026-03-27 17:45:33 -0400

    Bonus first draft

--- backend/app/db/models/bonus_model.py
+++ backend/app/db/models/bonus_model.py
@@ -1,9 +1,24 @@
 from app.db.db import Base
-from app.db.models.casino_model import Casino
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Boolean
+from sqlalchemy.orm import relationship as rel
+from datetime import datetime
 
 class Bonus(Base):
     __tablename__ = 'bonuses'
 
+    # Identification
     id = Column(Integer, primary_key=True)
-    name = Column(String, unique=True, nullable=False, default=f'{}')
+    name = Column(String, unique=True, nullable=False)
+
+    # Information
+    casino_id = Column(Integer, ForeignKey('casinos.id'))
+    bonus_type = Column(String, nullable=False)
+    bonus_value = Column(Integer, nullable=False)
+    location_sensitive = Column(Boolean, nullable=False)
+
+    # Relationships
+    casino = rel('Casino', back_populates='bonuses')
+
+    # Time stamp
+    discovery = Column(DateTime, nullable=False, default=datetime.now)
+    updated_at = Column(DateTime, nullable=False, default=datetime.now, onupdate=datetime.now)
